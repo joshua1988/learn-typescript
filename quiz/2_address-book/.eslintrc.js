50967893ee3c92590eecf86f727b0faa9ab052da
joshua1988 <jangkeehyo@gmail.com>
2020-04-17 18:58:11 +0900

    complete the types

--- quiz/2_address-book/.eslintrc.js
+++ quiz/2_address-book/.eslintrc.js
@@ -23,8 +23,8 @@ module.exports = {
         arrowParens: 'avoid',
       },
     ],
-    '@typescript-eslint/no-explicit-any': 'off',
-    "@typescript-eslint/explicit-function-return-type": 'off',
+    // '@typescript-eslint/no-explicit-any': 'off',
+    // "@typescript-eslint/explicit-function-return-type": 'off',
     'prefer-const': 'off',
   },
   parserOptions: {
