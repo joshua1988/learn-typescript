e63e5e610c13a0c6651e61a5e32501361768364b
joshua1988 <jangkeehyo@gmail.com>
2021-01-27 21:15:23 +0900

    add code to bundle the whole application to fix the module loading error

---
+++ project/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const webpack = require('webpack');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/app.ts',
+  output: {
+    filename: 'app.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  plugins: [new webpack.ProgressPlugin(), new CleanWebpackPlugin()],
+};
