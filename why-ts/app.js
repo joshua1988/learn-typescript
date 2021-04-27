c27e136d34effaaf58911e9efd8ddd19ac981532
Sunrise Gong <rhdgoehe2@gmail.com>
2021-04-27 22:54:01 +0900

    Try the tsc command

--- why-ts/app.js
+++ why-ts/app.js
@@ -8,14 +8,46 @@ var address = document.querySelector('#address');
 
 // user data
 var user = {};
+//-----------------------------------------------
+/**
+ * @typedef {object} Address
+ * @property {string} street
+ * @property {string} city
+ */
 
+/**
+ * @typedef {object} User
+ * @property {string} name
+ * @property {string} email
+ * @property {Address} address
+ */
+
+/**
+ * @returns {Promise<User>}
+ */
+
+//-----------------------------------------------
+function fetchUser() {
+  return axios.get(url);
+}
+
+fetchUser().then(function (response) {
+  response.address.city
+})
+
+//-----------------------------------------------
 function startApp() {
-  axios
-    .get(url)
+  // axios
+  //   .get(url)
+  fetchUser()
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address;
     })
     .catch(function (error) {
       console.log(error);
