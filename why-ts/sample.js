98c55c9b051f6ae8faa44f8033e2d262a82cd291
bmaner <bmanerdaniel@gmail.com>
2021-12-07 08:38:14 +0900

    Add ts example

---
+++ why-ts/sample.js
@@ -0,0 +1,20 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// // sum(10, '20'); //1020
+
+// @ts-check
+
+/**
+ *
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ * @returns
+ */
+
+function sum(a, b) {
+    return a + b;
+}
+
+// sum(10, '20');
