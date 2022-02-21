85217bf226f06dbdd47d92c6bd54de421e848eb7
KimMinchan95 <hialscks@gmail.com>
2022-02-21 23:47:58 +0900

    day 1

---
+++ why-ts/sample.js
@@ -0,0 +1,18 @@
+// function sum(a, b) {
+//     return a + b;
+// }
+
+// sum(10, '20');
+
+// @ts-check
+
+/**
+ * 
+ * @param {number} a 첫번째 숫자
+ * @param {number} b 두번째 숫자
+ */
+function sum1(a, b) {
+    return a + b;
+}
+
+sum1(10, '20');
---
+++ why-ts/sample.ts
@@ -0,0 +1,8 @@
+//function add(a: number, b: number): number {
+//    return a + b;
+//}
+
+//const result = add(10, 20);
+//result.toLocaleString();
+
+// @ts-check
