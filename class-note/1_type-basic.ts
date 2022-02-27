e9499cd0c26baac2a99e4708d4780307ecdff2c2
KimMinchan95 <hialscks@gmail.com>
2022-02-28 01:01:51 +0900

    day 3

---
+++ class-note/1_type-basic.ts
@@ -0,0 +1,16 @@
+// JS 문자열 선언
+// const str = 'hello';
+
+// TS 문자열 선언
+const str: string = 'hello';
+
+// TS 숫자 선언
+const num: number = 10;
+
+// 배열 - 첫 글자가 대문자
+const arr: Array<number> = [1, 2, 3];
+
+const heroes: Array<string> = ['Capt', 'Thor', 'Hulk'];
+
+// 배열을 아래처럼 선언해도 됨
+const items: number[] = [1, 2, 3];
